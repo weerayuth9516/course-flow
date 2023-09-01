615fa44dda93f073f4e5be750c994fa5c68c0ced
jirayut <jirayutrpy@hotmail.com>
2023-09-01 23:47:24 +0700

    feat:setup api and install extention lib

--- server/app.js
+++ server/app.js
@@ -1,12 +1,25 @@
 import express from "express";
+import bodyParser from "body-parser";
+import cors from "cors";
+import userRouter from "./apps/userRouter.js";
+import dotenv from "dotenv";
+async function init() {
+  const app = express();
+  const port = 4001;
+  dotenv.config();
+  app.use(cors());
+  app.use(bodyParser.json());
+  app.use("/users", userRouter);
 
-const app = express();
-const port = 4001;
+  app.get("/", (req, res) => {
+    return res.send("Course Flow API Running.");
+  });
+  app.get("*", (req, res) => {
+    return res.status(404).send("API Not found.");
+  });
+  app.listen(port, () => {
+    console.log(`Course Flow API Server is running at ${port}`);
+  });
+}
 
-app.get("/", (req, res) => {
-  res.send(`Let's make it happen together`);
-});
-
-app.listen(port, () => {
-  console.log(`Server is running at ${port}`);
-});
+init();
