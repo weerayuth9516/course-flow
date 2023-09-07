102bc4abe3d61d6a784af5be4ff98eeee3aa5844
Goy <saiphanas.k@gmail.com>
2023-09-07 22:09:41 +0800

    fix: add linked courses-lessons-sublessons api

--- server/apps/courseRouter.js
+++ server/apps/courseRouter.js
@@ -1,6 +1,7 @@
 import { Router } from "express";
 import { supabase } from "../utils/db.js";
 import "dotenv/config";
+
 const courseRouter = Router();
 
 courseRouter.get("/", async (req, res) => {
@@ -44,6 +45,7 @@ courseRouter.get("/:id", async (req, res) => {
   }
 });
 
+//courseDetailPage/BE sprint2Edited
 courseRouter.get("/:id/lessons/", async (req, res) => {
   try {
     const courseId = req.params.id;
@@ -67,6 +69,9 @@ courseRouter.get("/:id/lessons/", async (req, res) => {
       .from("lessons")
       .select("lesson_name, sub_lessons(*)")
       .eq("course_id", courseId);
+      .from("lessons")
+      .select("lesson_name, sub_lessons(*)")
+      .eq("course_id", courseId);
 
     if (error) {
       return res.status(500).json({ error: error.message });
@@ -76,6 +81,7 @@ courseRouter.get("/:id/lessons/", async (req, res) => {
       return res.status(404).json({ error: "Lesson not found" });
     }
 
+    const lessonData = data;
     const lessonData = data;
 
     return res.json({
@@ -87,85 +93,89 @@ courseRouter.get("/:id/lessons/", async (req, res) => {
   }
 });
 
-courseRouter.get("/:id/lessons/:lessonId", async (req, res) => {
+//myCoursesPage/BE sprint2
+
+courseRouter.get("/mycourses/:id", async (req, res) => {
   try {
-    const courseId = req.params.id;
-    const lessonId = req.params.lessonId;
+    // Retrieve the authenticated user's ID
+    const id = req.params.id;
 
-    const { data, error } = await supabase
-      .from("courses")
-      .select("*,lessons(*)")
-      .eq("course_id", courseId)
-      .eq("lessons.lesson_id", lessonId);
+    const results = await supabase.from("users").select("*").eq("user_id", id);
 
-    if (error) {
-      return res.status(500).json({ error: error.message });
-    }
-
-    if (data.length === 0 || !data[0].lessons) {
-      return res.status(404).json({ error: "Lesson not found" });
-    }
-
-    const lesson = data[0].lessons.find((l) => l.lesson_id === lessonId);
-
-    if (!lesson) {
-      return res.status(404).json({ error: "Lesson not found" });
-    }
-
-    return res.json({ lesson });
-  } catch (error) {
-    console.error("An error occurred:", error);
-    return res.status(500).json({ error: "Internal Server Error" });
-  }
-});
-
-courseRouter.get("/:id/lessons/:lessonId/sublessons", async (req, res) => {
-  try {
-    const courseId = req.params.id;
-    const lessonId = req.params.lessonId;
-
-    // Check if the course with courseId exists
-    // Check if the course with courseId exists
-    const { data: courseData, error: courseError } = await supabase
-      .from("courses")
+    // Query the database to fetch subscribed courses for the user
+    const { data: userCoursesData, error: userCoursesError } = await supabase
+      .from("user_course_details")
       .select("course_id")
-      .eq("course_id", courseId)
-      .single();
+      .eq("user_id", id);
 
-    if (courseError) {
-      return res.status(500).json({ error: courseError.message });
+    if (userCoursesError) {
+      return res.status(500).json({ error: userCoursesError.message });
     }
 
-    if (!courseData) {
-      return res.status(404).json({ error: "Course Id not found" });
+    if (!userCoursesData || userCoursesData.length === 0) {
+      return res.status(404).json({ error: "No subscribed courses found" });
     }
 
-    // Retrieve data for the specified lesson and its sub-lessons
-    const { data, error } = await supabase
-      .from("courses")
-      .select("*,lessons(*, sub_lessons(*))")
-      .eq("course_id", courseId)
-      .eq("lessons.lesson_id", lessonId);
+    const courseIds = userCoursesData.map((userCourse) => userCourse.course_id);
 
-    if (error) {
-      return res.status(500).json({ error: error.message });
+    // Query the database to fetch course details for the subscribed courses
+    const { data: subscribedCoursesData, error: subscribedCoursesError } =
+      await supabase
+        .from("courses")
+        .select("course_id", "course_name", "course_summary")
+        .in("course_id", courseIds);
+
+    if (subscribedCoursesError) {
+      return res.status(500).json({ error: subscribedCoursesError.message });
     }
 
-    if (!data || data.length === 0) {
-      return res.status(404).json({ error: "Lesson not found" });
+    if (!subscribedCoursesData || subscribedCoursesData.length === 0) {
+      return res.status(404).json({ error: "No subscribed courses found" });
     }
 
-    // Assuming data[0] contains the lesson and its sub-lessons
-    const lessonData = data[0];
+    const subscribedCoursesArray = subscribedCoursesData.map((course) => ({
+      course_id: course.course_id,
+      course_name: course.course_name,
+      course_summary: course.course_summary,
+    }));
 
-    // Now you can return the lessonData as a response
-    return res.json({
-      data: lessonData,
-    });
+    res.json(subscribedCoursesArray);
   } catch (error) {
     console.error("An error occurred:", error);
     return res.status(500).json({ error: "Internal Server Error" });
   }
 });
+
+// courseRouter.get("/:id/lessons/:lessonId", async (req, res) => {
+//   try {
+//     const courseId = req.params.id;
+//     const lessonId = req.params.lessonId;
+
+//     const { data, error } = await supabase
+//       .from("courses")
+//       .select("*,lessons(*)")
+//       .eq("course_id", courseId)
+//       .eq("lessons.lesson_id", lessonId);
+
+//     if (error) {
+//       return res.status(500).json({ error: error.message });
+//     }
+
+//     if (data.length === 0 || !data[0].lessons) {
+//       return res.status(404).json({ error: "Lesson not found" });
+//     }
+
+//     const lesson = data[0].lessons.find((l) => l.lesson_id === lessonId);
+
+//     if (!lesson) {
+//       return res.status(404).json({ error: "Lesson not found" });
+//     }
+
+//     return res.json({ lesson });
+//   } catch (error) {
+//     console.error("An error occurred:", error);
+//     return res.status(500).json({ error: "Internal Server Error" });
+//   }
+// });
 
 export default courseRouter;
