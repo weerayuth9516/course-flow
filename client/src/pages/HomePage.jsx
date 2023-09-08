a098f5fe6468b06494b4fad452388cdd977c1995
Drew <ardaz_drew@hotmail.com>
2023-09-08 14:03:56 +0700

    styling slideshow

--- client/src/pages/HomePage.jsx
+++ client/src/pages/HomePage.jsx
@@ -2,6 +2,13 @@ import React from "react";
 import Header from "../components/Header";
 import Footer from "../components/Footer";
 import { Link } from "react-router-dom";
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { Navigation, Pagination, Autoplay } from 'swiper/modules';
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+import 'swiper/css/scrollbar';
+import 'swiper/css/autoplay';
 
 import vector from "../assets/homepage/vector.png"
 import polygon from "../assets/homepage/Polygon.png"
@@ -21,14 +28,25 @@ import sec3 from "../assets/homepage/sec3.png"
 import sec4 from "../assets/homepage/sec4.png"
 import sec5 from "../assets/homepage/sec5.png"
 import sec6 from "../assets/homepage/sec6.png"
+import instructor1 from "../assets/homepage/instructor1.png"
+import instructor2 from "../assets/homepage/instructor2.png"
+import instructor3 from "../assets/homepage/instructor3.png"
+import programmer from "../assets/homepage/programmer.png"
+import programmer2 from "../assets/homepage/programmer2.png"
+import graduate1 from "../assets/homepage/graduate1.png"
+import graduate2 from "../assets/homepage/graduate2.png"
+
+
+
+
+
+
+
 
 export default function HomePage() {
   return <>
-
-    <div className="text-[48px] pl-[5%]">
-      <Header />
-    </div>
-    <div className="bg-blue-100 h-auto relative pl-[15%] pr-[15%] mb-[100px]">
+    <Header />
+    <div className="bg-blue-100 h-auto relative pl-[15%] pr-[15%] mb-[200px]">
       <div className="pt-[210px] pb-[230px]">
         <div className="font-semibold text-5xl">Best Virtual</div>
         <div className="font-semibold text-5xl mb-[25px]">Classroom Software</div>
@@ -47,17 +65,17 @@ export default function HomePage() {
       </div>
 
     </div>
-    <div className="flex pl-[15%] pr-[15%] gap-[50px] mb-[100px] relative">
-      <img className="absolute top-[-34%] left-[15%]" src={sec2}></img>
+    <div className="flex pl-[15%] pr-[15%] mb-[100px] relative">
+      <img className="absolute top-[-67%] left-[15%]" src={sec2}></img>
       <img className="absolute top-[-20%] left-[35%]" src={sec3}></img>
       <img className="absolute top-[70%] right-[5%]" src={sec4}></img>
-      <img className="object-cover h-auto w-[40%] rounded-md"
-        src="https://www.phoenix.edu/content/dam/edu/blog/2023/02/Male-programmer-writing-code-in-modern-office-704x421.jpg"
+      <img className="object-cover h-auto w-[40%] rounded-md mr-[10%]"
+        src={programmer}
         alt=""></img>
       <div className="flex flex-col">
 
-        <div className="font-semibold text-2xl">Learning experience has been</div>
-        <div className="mb-[30px] font-semibold text-2xl">enhanced with new technologies</div>
+        <div className="font-semibold text-header2">Learning experience has been</div>
+        <div className="mb-[30px] font-semibold text-header2">enhanced with new technologies</div>
         <div className="flex mb-[20px]">
           <img className="mr-[15px] h-[30px] w-[30px]"
             src={verify}
@@ -67,6 +85,7 @@ export default function HomePage() {
             <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nesciunt aliquam voluptate vitae
               nihil,
               eligendi ab aliquid.</p>
+
 
           </div>
 
@@ -93,10 +112,10 @@ export default function HomePage() {
 
       <div className="flex flex-col">
 
-        <div className="font-semibold text-2xl">Interaction between the tutor </div>
-        <div className="mb-[30px] font-semibold text-2xl">and the learners</div>
+        <div className="font-semibold text-header2">Interaction between the tutor </div>
+        <div className="mb-[30px] font-semibold text-header2">and the learners</div>
         <div className="flex mb-[20px]">
-          <img className="mr-[15px] h-[30px] w-[30px]"
+          <img className="mr-[15px] h-[30px] w-[30px] "
             src={commu}
             alt=""></img>
           <div className="flex-col">
@@ -122,53 +141,142 @@ export default function HomePage() {
 
         </div>
       </div>
-      <img className="object-cover h-auto w-[40%] rounded-md"
-        src="https://usa.bootcampcdn.com/wp-content/uploads/sites/119/2020/12/tes_gen_blog_code7-1-800x412.jpg"
+      <img className="object-cover h-auto w-[40%] rounded-md ml-[10%]"
+        src={programmer2}
         alt=""></img>
 
     </div>
 
     <div className=" flex flex-col justify-center items-center">
-      <h1 className="items-center justify-center mb-[5px] text-2xl text-bold font-semibold">Our Professional Instructor</h1>
+      <h1 className="items-center justify-center mb-[5px] text-header2 text-bold font-semibold">
+        Our Professional Instructor
+      </h1>
       <div className="pl-[10%] pr-[10%] flex gap-[20px] mb-[200px]">
         <div className="flex flex-col justify-center items-center">
-          <img className="w-[350px] h-[65%] object-cover rounded-md"
-            src="https://w.wallha.com/ws/14/DWaxqUMB.jpg"
+          <img className="w-[300px] h-[80%] object-cover rounded-md"
+            src={instructor1}
             alt=""></img>
-          <div>Kim Jisoo</div>
-          <div className="text-xs text-blue-400">Blackpink</div>
+          <div>Jane Cooper</div>
+          <div className="text-xs text-blue-400">UX/UI Designer</div>
         </div>
         <div className="flex flex-col justify-center items-center">
-          <img className="w-[350px] h-[65%] object-cover rounded-md"
-            src="https://s.isanook.com/jo/0/ud/492/2462673/jennie.jpg?ip/crop/w670h402/q80/jpg"
+          <img className="w-[300px] h-[80%] object-cover rounded-md"
+            src={instructor2}
             alt=""></img>
-          <div>Kim Jennie</div>
-          <div className="text-xs text-blue-400">Blackpink</div>
+          <div>Esther Howard</div>
+          <div className="text-xs text-blue-400">Programmer Manager</div>
         </div>
         <div className="flex flex-col justify-center items-center">
-          <img className="w-[350px] h-[65%] object-cover rounded-md"
-            src="https://i.mydramalist.com/66L5p_5c.jpg"
+          <img className="w-[300px] h-[80%] object-cover rounded-md"
+            src={instructor3}
             alt=""></img>
-          <div>Park Chaeyoung</div>
-          <div className="text-xs text-blue-400">Blackpink</div>
+          <div>Brooklyn Simmons</div>
+          <div className="text-xs text-blue-400">Software Engineer</div>
         </div>
-        <div className="flex flex-col justify-center items-center">
-          <img className="w-[350px] h-[65%] object-cover rounded-md"
-            src="https://i.pinimg.com/736x/16/34/5b/16345b49fe985982921dabaefa03880c.jpg"
-            alt=""></img>
-          <div>Lalisa Manoban</div>
-          <div className="text-xs text-blue-400">Blackpink</div>
-        </div>
+
 
 
       </div>
 
     </div>
-    <section className="mb-[200px] relative">
-      <img className="w-screen" src={frame}></img>
-      <img className="absolute right-[5%] top-[-10%]" src={sec3}></img>
-      <img className="absolute right-0 top-[-30%]" src={sec6}></img>
-    </section>
+    {/* <section className="mb-[200px] relative flex flex-col items-center justify-center w-screen overflow-hidden"> */}
+
+    <div className="text-header2 font-semibold mb-[50px] flex items-center justify-center">Our Graduates</div>
+
+    {/* <Swiper
+      // modules={[Navigation, Pagination, Autoplay]}
+      // slidesPerView={3}
+      // navigation
+      // autoplay={{ delay: 2000 }}
+      // pagination={{ clickable: true }}
+      navigation={true}
+      modules={[Navigation]}
+      className="mySwiper w-[100%] h-[100%]"
+    > */}
+
+    {/* <SwiperSlide className="text-center flex justify-center items-center">
+      <div className="bg-blue-100 w-[738px] h-[312px] py-[5%] pl-[7%] pr-[20px] relative block">
+        <div className="mb-[10px] font-semibold text-blue-500 text-xl">Saiful Islam</div>
+        <p className="text-gray-700">Start with something simple and small, then expand over time.</p>
+        <p className="text-gray-700">If people call it a ‘toy’ you’re definitely onto something.</p>
+        <p className="text-gray-700">If you’re waiting for encouragement from others, you’re doing it</p>
+        <p className="text-gray-700">wrong. By the time people think an idea is good, it’s probably</p>
+        <p className="text-gray-700">too late.</p>
+        <img className="absolute top-[10%] left-[-25%] " src={graduate2}></img>
+      </div>
+    </SwiperSlide>
+    <SwiperSlide className="text-center flex justify-center items-center">
+      <div className="bg-blue-100 w-[738px] h-[312px] py-[5%] pl-[7%] pr-[20px] relative">
+        <div className="mb-[10px] font-semibold text-blue-500 text-xl">Saiful Islam</div>
+        <p className="text-gray-700">Start with something simple and small, then expand over time.</p>
+        <p className="text-gray-700">If people call it a ‘toy’ you’re definitely onto something.</p>
+        <p className="text-gray-700">If you’re waiting for encouragement from others, you’re doing it</p>
+        <p className="text-gray-700">wrong. By the time people think an idea is good, it’s probably</p>
+        <p className="text-gray-700">too late.</p>
+        <img className="absolute top-[10%] left-[-25%]" src={graduate1}></img>
+      </div>
+    </SwiperSlide>
+    <SwiperSlide className="text-center flex justify-center items-center">
+      <div className="bg-blue-100 w-[738px] h-[312px] py-[5%] pl-[7%] pr-[20px] relative">
+        <div className="mb-[10px] font-semibold text-blue-500 text-xl">Saiful Islam</div>
+        <p className="text-gray-700">Start with something simple and small, then expand over time.</p>
+        <p className="text-gray-700">If people call it a ‘toy’ you’re definitely onto something.</p>
+        <p className="text-gray-700">If you’re waiting for encouragement from others, you’re doing it</p>
+        <p className="text-gray-700">wrong. By the time people think an idea is good, it’s probably</p>
+        <p className="text-gray-700">too late.</p>
+        <img className="absolute top-[10%] left-[-25%]" src={graduate2}></img>
+      </div>
+    </SwiperSlide>
+
+  </Swiper > */}
+
+    {/* <img className="absolute right-[5%] top-[-10%]" src={sec3}></img>
+    <img className="absolute right-0 top-[-30%]" src={sec6}></img> */}
+
+
+
+    <Swiper navigation={true} modules={[Navigation]} className="mySwiper w-[80%] mb-[200px]">
+      <SwiperSlide className="flex justify-center items-center w-[50%]">
+        <div className="bg-blue-100 w-[738px] h-[312px] py-[5%] pl-[7%] pr-[20px] relative ">
+          <div className="mb-[10px] font-semibold text-blue-500 text-xl">Saiful Islam</div>
+          <p className="text-gray-700">Start with something simple and small, then expand over time.</p>
+          <p className="text-gray-700">If people call it a ‘toy’ you’re definitely onto something.</p>
+          <p className="text-gray-700">If you’re waiting for encouragement from others, you’re doing it</p>
+          <p className="text-gray-700">wrong. By the time people think an idea is good, it’s probably</p>
+          <p className="text-gray-700">too late.</p>
+          <img className="absolute top-[10%] left-[-25%] " src={graduate2}></img>
+        </div>
+      </SwiperSlide>
+      <SwiperSlide className="flex justify-center items-center">
+        <div className="bg-blue-100 w-[738px] h-[312px] py-[5%] pl-[7%] pr-[20px] relative">
+          <div className="mb-[10px] font-semibold text-blue-500 text-xl">Saiful Islam</div>
+          <p className="text-gray-700">Start with something simple and small, then expand over time.</p>
+          <p className="text-gray-700">If people call it a ‘toy’ you’re definitely onto something.</p>
+          <p className="text-gray-700">If you’re waiting for encouragement from others, you’re doing it</p>
+          <p className="text-gray-700">wrong. By the time people think an idea is good, it’s probably</p>
+          <p className="text-gray-700">too late.</p>
+          <img className="absolute top-[10%] left-[-25%]" src={graduate1}></img>
+        </div>
+      </SwiperSlide>
+      <SwiperSlide className="flex justify-center items-center">
+        <div className="bg-blue-100 w-[738px] h-[312px] py-[5%] pl-[7%] pr-[20px] relative">
+          <div className="mb-[10px] font-semibold text-blue-500 text-xl">Saiful Islam</div>
+          <p className="text-gray-700">Start with something simple and small, then expand over time.</p>
+          <p className="text-gray-700">If people call it a ‘toy’ you’re definitely onto something.</p>
+          <p className="text-gray-700">If you’re waiting for encouragement from others, you’re doing it</p>
+          <p className="text-gray-700">wrong. By the time people think an idea is good, it’s probably</p>
+          <p className="text-gray-700">too late.</p>
+          <img className="absolute top-[10%] left-[-25%]" src={graduate2}></img>
+        </div>
+      </SwiperSlide>
+      {/* <SwiperSlide>Slide 4</SwiperSlide>
+      <SwiperSlide>Slide 5</SwiperSlide>
+      <SwiperSlide>Slide 6</SwiperSlide>
+      <SwiperSlide>Slide 7</SwiperSlide>
+      <SwiperSlide>Slide 8</SwiperSlide>
+      <SwiperSlide>Slide 9</SwiperSlide> */}
+    </Swiper>
+    {/* </section> */}
 
 
     <div class="bg-gradient-to-r from-indigo-500 to-sky-500 h-auto relative flex justify-between">
