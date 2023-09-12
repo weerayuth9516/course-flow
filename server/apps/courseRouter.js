4be31951c7b135c3164a6c31cdbf42cc0533ae01
Goy <saiphanas.k@gmail.com>
2023-09-12 16:20:20 +0800

    feat: get in_progress status

--- server/apps/courseRouter.js
+++ server/apps/courseRouter.js
@@ -69,9 +69,6 @@ courseRouter.get("/:id/lessons/", async (req, res) => {
       .from("lessons")
       .select("lesson_name, sub_lessons(*)")
       .eq("course_id", courseId);
-      .from("lessons")
-      .select("lesson_name, sub_lessons(*)")
-      .eq("course_id", courseId);
 
     if (error) {
       return res.status(500).json({ error: error.message });
@@ -82,7 +79,6 @@ courseRouter.get("/:id/lessons/", async (req, res) => {
     }
 
     const lessonData = data;
-    const lessonData = data;
 
     return res.json({
       data: lessonData,
@@ -92,90 +88,154 @@ courseRouter.get("/:id/lessons/", async (req, res) => {
     return res.status(500).json({ error: "Internal Server Error" });
   }
 });
-
+//here
 //myCoursesPage/BE sprint2
-
-courseRouter.get("/mycourses/:id", async (req, res) => {
-  try {
-    // Retrieve the authenticated user's ID
-    const id = req.params.id;
-
-    const results = await supabase.from("users").select("*").eq("user_id", id);
-
-    // Query the database to fetch subscribed courses for the user
-    const { data: userCoursesData, error: userCoursesError } = await supabase
-      .from("user_course_details")
-      .select("course_id")
-      .eq("user_id", id);
-
-    if (userCoursesError) {
-      return res.status(500).json({ error: userCoursesError.message });
-    }
-
-    if (!userCoursesData || userCoursesData.length === 0) {
-      return res.status(404).json({ error: "No subscribed courses found" });
-    }
-
-    const courseIds = userCoursesData.map((userCourse) => userCourse.course_id);
-
-    // Query the database to fetch course details for the subscribed courses
-    const { data: subscribedCoursesData, error: subscribedCoursesError } =
-      await supabase
-        .from("courses")
-        .select("course_id", "course_name", "course_summary")
-        .in("course_id", courseIds);
-
-    if (subscribedCoursesError) {
-      return res.status(500).json({ error: subscribedCoursesError.message });
-    }
-
-    if (!subscribedCoursesData || subscribedCoursesData.length === 0) {
-      return res.status(404).json({ error: "No subscribed courses found" });
-    }
-
-    const subscribedCoursesArray = subscribedCoursesData.map((course) => ({
-      course_id: course.course_id,
-      course_name: course.course_name,
-      course_summary: course.course_summary,
-    }));
-
-    res.json(subscribedCoursesArray);
-  } catch (error) {
-    console.error("An error occurred:", error);
-    return res.status(500).json({ error: "Internal Server Error" });
-  }
-});
-
-// courseRouter.get("/:id/lessons/:lessonId", async (req, res) => {
+// ทำต่อ courseRouter.get("/:userId/mycourses", async (req, res) => {
 //   try {
-//     const courseId = req.params.id;
-//     const lessonId = req.params.lessonId;
+//     const userId = req.params.userId;
 
+//     // Query the user_course_details table to fetch courses in progress for the user
 //     const { data, error } = await supabase
-//       .from("courses")
-//       .select("*,lessons(*)")
-//       .eq("course_id", courseId)
-//       .eq("lessons.lesson_id", lessonId);
+//       .from("user_course_details")
+//       .select("*")
+//       .eq("user_id", userId);
 
 //     if (error) {
 //       return res.status(500).json({ error: error.message });
 //     }
 
-//     if (data.length === 0 || !data[0].lessons) {
-//       return res.status(404).json({ error: "Lesson not found" });
+//     // Filter the in-progress courses based on the subscription_id
+//     const in_progress = data.filter((user_course_detail_obj) => {
+//       return user_course_detail_obj.subscription_id === "2";
+//     });
+
+//     // Extract the courseIds from in-progress courses
+//     const courseIds = in_progress.map((course) => course.course_id);
+
+//     // Query the courses table to fetch details of the in-progress courses
+//     const { data: courseDetailData, error: courseError } = await supabase
+//       .from("courses")
+//       .select("course_name", "course_summary", "course_duration")
+//       .in("course_id", courseIds);
+
+//     if (courseError) {
+//       return res.status(500).json({ error: courseError.message });
 //     }
 
-//     const lesson = data[0].lessons.find((l) => l.lesson_id === lessonId);
-
-//     if (!lesson) {
-//       return res.status(404).json({ error: "Lesson not found" });
-//     }
-
-//     return res.json({ lesson });
+//     return res.json({
+//       in_progress: in_progress,
+//       course_details: courseDetailData,
+//     });
 //   } catch (error) {
 //     console.error("An error occurred:", error);
 //     return res.status(500).json({ error: "Internal Server Error" });
 //   }
 // });
+
+//here
+// courseRouter.get("/:userId/mycourses", async (req, res) => {
+//   try {
+//     const userId = req.params.userId;
+
+//     // Query the user_course_details table to fetch courses in progress for the user
+//     const { data, error } = await supabase
+//       .from("user_course_details")
+//       .select("*")
+//       .eq("user_id", userId);
+
+//     if (error) {
+//       return res.status(500).json({ error: error.message });
+//     }
+
+//     // Filter the in-progress courses based on the subscription_id
+//     const in_progress = data.filter((user_course_detail_obj) => {
+//       return (
+//         user_course_detail_obj.subscription_id ===
+//         "46004adc-8589-4254-878f-c03a6ae7cea2"
+//       );
+//     });
+
+//     // Extract the courseIds from in-progress courses
+//     const courseIds = in_progress.map((course) => course.course_id);
+
+//     // Query the courses table to fetch details of the in-progress courses
+//     const { data: courseDetailData, error: courseError } = await supabase
+//       .from("courses")
+//       .select("course_name", "course_summary", "course_duration")
+//       .in("course_id", courseIds);
+
+//     if (courseError) {
+//       return res.status(500).json({ error: courseError.message });
+//     }
+
+//     return res.json({
+//       in_progress: in_progress,
+//       course_details: courseDetailData,
+//     });
+//   } catch (error) {
+//     console.error("An error occurred:", error);
+//     return res.status(500).json({ error: "Internal Server Error" });
+//   }
+// });
+
+//myCoursesPage/BE sprint2 in_progress postman
+courseRouter.get("/:userId/mycourses", async (req, res) => {
+  try {
+    const userId = req.params.userId;
+
+    // Query the user_course_details table to fetch courses in progress for the user
+    const { data, error } = await supabase
+      .from("user_course_details")
+      .select("*")
+      .eq("user_id", userId);
+
+    // .like("status_status", "%in_progress%");
+    const in_progress = data.filter((user_course_detail_obj) => {
+      return (
+        // user_course_detail_obj.subscription_status === "subscribed_course"
+        user_course_detail_obj.subscription_id === 1
+      );
+    });
+    return res.json({
+      in_progress,
+    });
+
+    // if (error) {
+    //   return res.status(500).json({ error: error.message });
+    // }
+
+    // if (!data || data.length === 0) {
+    //   return res
+    //     .status(404)
+    //     .json({ error: "No courses in progress found for the user" });
+    // }
+
+    // // Extract course IDs from the data
+    // const courseIds = data.map((userCourse) => userCourse.course_id);
+
+    // // Query the database to fetch details of the subscribed courses
+    // const { data: courseDetailData, error: courseError } = await supabase
+    //   .from("courses")
+    //   .select("course_name", "course_summary", "course_duration")
+    //   .eq("course_id", courseIds);
+
+    // if (courseError) {
+    //   return res.status(500).json({ error: courseError.message });
+    // }
+
+    // if (!courseDetailData || courseDetailData.length === 0) {
+    //   return res
+    //     .status(404)
+    //     .json({ error: "No course details found for the user" });
+    // }
+
+    // return res.json({
+    //   data: courseDetailData,
+    // });
+  } catch (error) {
+    console.error("An error occurred:", error);
+    return res.status(500).json({ error: "Internal Server Error" });
+  }
+});
 
 export default courseRouter;
