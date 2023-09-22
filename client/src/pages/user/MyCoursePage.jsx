b6d82c08690f950aa29c04c437aa46c1e4df15b4
Yanee Wiboonsak <dreambee11@gmail.com>
2023-09-22 13:21:54 +0700

    fix style

--- client/src/pages/user/MyCoursePage.jsx
+++ client/src/pages/user/MyCoursePage.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { useEffect } from "react";
+import { useState, useEffect } from "react";
 import DisplayCards from "../../components/user/DisplayCards";
 import Header from "../../components/Header";
 import Footer from "../../components/Footer";
@@ -18,6 +18,7 @@ function MyCoursePage() {
     setUserId,
     getAllMyCourses,
   } = useMycourses();
+  const [getFocus, setGetFocus] = useState(true);
 
   const auth = useAuth();
 
@@ -45,17 +46,18 @@ function MyCoursePage() {
             <button
               onClick={() => {
                 setStatus("myCourses");
+                setGetFocus(true);
               }}
-              // className="transform  transition-transform duration-300 ease-in-out "
-              className="transform transition-transform duration-300 ease-in-out hover:border-b hover:border-b-1 hover:text-black text-gray-600 focus:text-black focus:border-b focus:border-b-1 focus: border-black p-2"
-              // autoFocus="border-b border-b-1"
-              // className = "transform transition-transform duration-300 ease-in-out border-b border-b-1 text-black"
+              className={`text-black p-2 ${
+                getFocus ? "border-b border-black " : "text-gray-600"
+              } transform transition-transform duration-300 ease-in-out  hover:border-b hover:border-b-1 hover:text-black focus:text-black focus:border-b focus:border-b-1 focus:border-black`}
             >
               All Course
             </button>
             <button
               onClick={() => {
                 setStatus("in_progress");
+                setGetFocus(false);
               }}
               className="transform  transition-transform duration-300 ease-in-out hover:border-b hover:border-b-1 hover:text-black text-gray-600 focus:text-black focus:border-b focus:border-b-1 focus: border-black p-2"
             >
@@ -64,6 +66,7 @@ function MyCoursePage() {
             <button
               onClick={() => {
                 setStatus("completed");
+                setGetFocus(false);
               }}
               className="transform  transition-transform duration-300 ease-in-out hover:border-b hover:border-b-1 hover:text-black text-gray-600 focus:text-black focus:border-b focus:border-b-1 focus: border-black p-2"
             >
