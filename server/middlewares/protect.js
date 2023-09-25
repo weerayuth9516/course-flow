4a444511541b66218621309ba9f650dc7c40f887
jirayut <jirayutrpy@hotmail.com>
2023-09-25 13:53:03 +0700

    refactor

--- server/middlewares/protect.js
+++ server/middlewares/protect.js
@@ -1,10 +1,17 @@
 import jwt from "jsonwebtoken";
+import multer from "multer";
 export const protect = async (req, res, next) => {
   const token = req.headers.authorization;
   if (!token || !token.startsWith(`Bearer `)) {
     return res.status(401).json({
       message: "Token has invlid format",
     });
+  }
+  if (
+    req.headers["content-type"].split(" ")[0].split(";")[0] ===
+    "multipart/form-data"
+  ) {
+    console.log(req.headers["content-type"].split(" ")[0].split(";")[0]);
   }
   const pureToken = token.split(" ")[1];
   jwt.verify(pureToken, process.env.SECRET_KEY, (err, payload) => {
