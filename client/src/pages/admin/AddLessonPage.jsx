66e1869bdf4583b3bd608836c46426fa2b1228ac
Weerayuth Aridej <135725986+weerayuth9516@users.noreply.github.com>
2023-09-25 13:30:58 +0700

    fix: rebase conflict

--- client/src/pages/admin/AddLessonPage.jsx
+++ client/src/pages/admin/AddLessonPage.jsx
@@ -6,18 +6,13 @@ import LessonForm from "../../components/admin/LessonForm";
 function AddLessonPage() {
   return (
     <>
-<<<<<<< HEAD
       <div className="flex">
         <Sidebar />
         <div className="flex flex-col w-screen bg-gray-100 items-center">
-          <div className="h-[70px] w-full border-gray-500 border-b">navbar</div>
+          <Link to="/admin/addcourse"><div className="h-[70px] w-full border-gray-500 border-b">navbar | back to addCourse</div></Link>
           <LessonForm />
         </div>
       </div>
-=======
-    <div>AddLessonPage</div>
-    <Link to="/admin/addcourse">Back to AddCoursePage</Link>
->>>>>>> c9e6b8a (basic test)
     </>
   );
 }
